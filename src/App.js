e8c832d8c7871c6ba86b022bd6bbb033f2c8dcaf
Beatrice <beatrice.c.kwan@gmail.com>
2022-03-07 18:14:17 -0500

    replace default with basic title and image container sections

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Zesty</h1>
+        <h2>A photography gallery <br /> by Beatrice Kwan </h2>
+      </div>
+
+      <div className="img-container">
+        <img alt="" src="" />
+      </div>
     </div>
   );
 }
